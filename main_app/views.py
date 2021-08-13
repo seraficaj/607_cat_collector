c93d1cfd114d6b09e0a404151161d8be6d8ba99f
Jason Serafica <seraficaj@gmail.com>
2021-08-13 09:14:58 -0700

    Cat Collectors, index page, Class Cats in views.py

--- main_app/views.py
+++ main_app/views.py
@@ -5,9 +5,26 @@ from django.shortcuts import render
 # Add the following import
 from django.http import HttpResponse
 
+# Add the Cat class & list and view function below the imports
+class Cat:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, name, breed, description, age):
+    self.name = name
+    self.breed = breed
+    self.description = description
+    self.age = age
+
+cats = [
+  Cat('Lolo', 'tabby', 'foul little demon', 3),
+  Cat('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
+  Cat('Raven', 'black tripod', '3 legged cat', 4)
+]
+
 # Define the home view
 def home(request):
     return HttpResponse('<h1>Hewwo. I am your Django App. /ᐠUwUᐟ\ﾉ.</h1>')
 
 def about(request):
     return render(request, 'about.html')
+
+def cats_index(request):
+    return render(request, 'cats/index.html', {'cats': cats})
