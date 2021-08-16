2cf861f75eecbb32b2ce65dd7ad103e92d94f1a7
Jason Serafica <seraficaj@gmail.com>
2021-08-16 14:28:48 -0700

    CRUD with class based views for cats

--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Cat
 
 # Class Based Views
 class CatCreate(CreateView):
     model = Cat
-    fields = '__all__'
+    fields = "__all__"
+    success_url = "/cats/"
+
+
+class CatUpdate(UpdateView):
+    model = Cat
+    # Let's disallow the renaming of a cat by excluding the name field!
+    fields = ["breed", "description", "age"]
+
+
+class CatDelete(DeleteView):
+    model = Cat
+    success_url = "/cats/"
+
 
 # Define view functions
 def home(request):
@@ -22,5 +35,5 @@ def cats_index(request):
 
 
 def cats_detail(request, cat_id):
-  cat = Cat.objects.get(id=cat_id)
-  return render(request, 'cats/detail.html', {'cat': cat})
+    cat = Cat.objects.get(id=cat_id)
+    return render(request, "cats/detail.html", {"cat": cat})
