717a754664c28752aef1ce4c4b97fbcb7255b4c5
Jason Serafica <seraficaj@gmail.com>
2021-08-16 09:17:07 -0700

    refactor View All My Cats to use Cat Model

--- main_app/views.py
+++ main_app/views.py
@@ -1,27 +1,15 @@
 from django.shortcuts import render
-
-# Create your views here.
-
-# Add the Cat class & list and view function below the imports
-class Cat:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, breed, description, age):
-    self.name = name
-    self.breed = breed
-    self.description = description
-    self.age = age
-
-cats = [
-  Cat('Lolo', 'tabby', 'foul little demon', 3),
-  Cat('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
-  Cat('Raven', 'black tripod', '3 legged cat', 4)
-]
+from .models import Cat
 
 # Define the home view
 def home(request):
-    return render(request, 'home.html')
+    return render(request, "home.html")
+
 
 def about(request):
-    return render(request, 'about.html')
+    return render(request, "about.html")
+
 
 def cats_index(request):
-    return render(request, 'cats/index.html', {'cats': cats})
+    cats = Cat.objects.all()
+    return render(request, "cats/index.html", {"cats": cats})
